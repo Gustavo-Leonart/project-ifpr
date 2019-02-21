3163f629e8f71d3fba12f12ead024073f821a2c8
Gustavo-Leonart <42281223+Gustavo-Leonart@users.noreply.github.com>
2019-02-21 15:20:08 -0300

    ajustes e banner finalizado

---
+++ projeto-ifpr/software/old/automaticSlides.js
@@ -0,0 +1,26 @@
+//window.alert("hello!");
+var slideIndex = 0;
+showSlides(slideIndex);
+
+$(document).ready(function(n){ //mostra apenas uma imagem do "slide" na tela por vez
+    showSlides(slideIndex += n);
+});
+
+function forwardSlides(n){
+  showSlides(slideIndex += n);
+}
+function showSlides(n){
+  var s = document.getElementsByClassName("plusSlides"); //pega as classes com o nome "plusSlides"
+  var i;
+  //console.log(s); //gera um log (texto) da variável "s"
+
+  	for(i = 0; i < s.length; i++){
+    	s[i].style.display = "none";
+  	}
+  	slideIndex++;
+  	if(slideIndex > s.length){
+  		slideIndex = 1;
+  	}
+  	s[slideIndex-1].style.display = "block";
+  	setTimeout(showSlides, 8500); //tempo de mudança dos slides
+}
