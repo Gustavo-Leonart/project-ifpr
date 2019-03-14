42c1d2a41f1da35f72549179f5afef5f57795375
Gustavo-Leonart <42281223+Gustavo-Leonart@users.noreply.github.com>
2019-03-14 18:53:28 -0300

    ajustes

--- projeto-ifpr/software/updatePedido.php
+++ projeto-ifpr/software/updatePedido.php
@@ -5,11 +5,9 @@ $id_menu = $_POST["id_menu"];
 $data_entrega = $_POST["data_entrega"];
 $status_pedido = $_POST["status_pedido"];
 $valor = $_POST["valor"];
-if (!isset($_SESSION)) session_start();
-echo $success["success"];
 
 $update = mysqli_query($conexao, "UPDATE pedido set id_menu = $id_menu,
     data_entrega = '$data_entrega', status_pedido = $status_pedido, valor = $valor
     WHERE id_pedido = $id");
-    header("Location:editAgen.php?id_pedido=$id");
+    header("Location:editAgen.php?id_pedido=$id&alterado");
 ?>
